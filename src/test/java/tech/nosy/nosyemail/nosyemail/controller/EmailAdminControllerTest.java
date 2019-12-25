af259482cc58b5000ce8552679114da27ae65ac4
oktayalizada <oktay.alizada@gmail.com>
2019-12-25 19:43:51 +0100

    Add test

--- src/test/java/tech/nosy/nosyemail/nosyemail/controller/EmailAdminControllerTest.java
+++ src/test/java/tech/nosy/nosyemail/nosyemail/controller/EmailAdminControllerTest.java
@@ -1,206 +1,126 @@
-//package tech.nosy.nosyemail.nosyemail.controller;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.springframework.http.HttpStatus;
-//import tech.nosy.nosyemail.nosyemail.dto.EmailTemplateDto;
-//import tech.nosy.nosyemail.nosyemail.dto.FeedDto;
-//import tech.nosy.nosyemail.nosyemail.dto.InputSystemDto;
-//import tech.nosy.nosyemail.nosyemail.model.EmailProviderProperties;
-//import tech.nosy.nosyemail.nosyemail.model.EmailTemplate;
-//import tech.nosy.nosyemail.nosyemail.model.ReadyEmail;
-//import tech.nosy.nosyemail.nosyemail.service.EmailTemplateService;
-//import tech.nosy.nosyemail.nosyemail.service.FeedService;
-//import tech.nosy.nosyemail.nosyemail.service.InputSystemService;
-//
-//import java.security.Principal;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.Mockito.mock;
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class EmailAdminControllerTest {
-//
-//    @InjectMocks
-//    EmailAdminController emailAdminController;
-//
-//    @Mock
-//    EmailTemplateService emailTemplateService;
-//
-//    @Mock
-//    InputSystemService inputSystemService;
-//
-//    @Mock
-//    FeedService feedService;
-//
-//    @Test
-//    public void emailTemplatePost() {
-//        EmailTemplateDto emailTemplateDto=new EmailTemplateDto();
-//        emailTemplateDto.setSubject("TestSubject");
-//        Principal principal=mock(Principal.class);
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                emailTemplatePost("dasda", "dasdas", null,
-//                        principal).getStatusCode());
-//        assertEquals("TestSubject",emailTemplateDto.getSubject());
-//    }
-//
-//    @Test
-//    public void emailPost() {
-//        ReadyEmail readyEmail = new ReadyEmail();
-//        readyEmail.setEmailTemplate(new EmailTemplate());
-//        readyEmail.getEmailTemplate().setEmailTemplateSubject("TestSubject");
-//
-//        assertEquals(HttpStatus.OK, emailAdminController.emailPost(readyEmail).getStatusCode());
-//    }
-//
-//    @Test
-//    public void newType() {
-//        InputSystemDto inputSystemDto=new InputSystemDto();
-//        Principal principal=mock(Principal.class);
-//        assertEquals(HttpStatus.CREATED, emailAdminController.newType(inputSystemDto,principal).getStatusCode());
-//
-//    }
-//
-//    @Test
-//    public void getInputSystems() {
-//        Principal principal=mock(Principal.class);
-//        assertEquals(HttpStatus.OK, emailAdminController.getInputSystems(principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void getEmailAllProviders() {
-//        Principal principal=mock(Principal.class);
-//        when(emailTemplateService.getAllEmailProviders()).thenReturn(null);
-//        assertEquals(HttpStatus.OK, emailAdminController.getEmailAllProviders(principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void updateInputSystemName() {
-//        Principal principal=mock(Principal.class);
-//        assertEquals(HttpStatus.OK, emailAdminController.updateInputSystemName("inputSystemId", null, principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void newEmailTemplate() {
-//        Principal principal=mock(Principal.class);
-//        assertEquals(HttpStatus.CREATED, emailAdminController.
-//                newEmailTemplate(principal, "inputSystemId",null).getStatusCode());
-//    }
-//
-//    @Test
-//    public void getEmailTemplateByInputSystemAndEmailTemplateId() {
-//        Principal principal=mock(Principal.class);
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                getEmailTemplateByInputSystemAndEmailTemplateId("emailTemplateId","inputSystemId",principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void updateEmailTemplate() {
-//        Principal principal=mock(Principal.class);
-//
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                updateEmailTemplate( "inputSystemId","emailTemplateId", null, principal)
-//                .getStatusCode());
-//    }
-//
-//
-//    @Test
-//    public void getEmailTemplates() {
-//        Principal principal=mock(Principal.class);
-//
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                getEmailTemplates("inputSystemId", principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void deleteEmailTemplate() {
-//        Principal principal=mock(Principal.class);
-//
-//        assertEquals(HttpStatus.NO_CONTENT, emailAdminController.
-//                deleteEmailTemplate("inputSystemId", "emailTemplateId",principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void deleteInputSystem() {
-//        Principal principal=mock(Principal.class);
-//
-//        assertEquals(HttpStatus.NO_CONTENT, emailAdminController.
-//                deleteInputSystem("inputSystemId", principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void newFeed() {
-//        FeedDto feedDto = new FeedDto();
-//        feedDto.setId("feedId");
-//        feedDto.setName("feedName");
-//        Principal principal = mock(Principal.class);
-//
-//        assertEquals(HttpStatus.CREATED, emailAdminController.
-//                newFeed("inputSystemId", feedDto, principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void updateFeedTest() {
-//        FeedDto feedDto = new FeedDto();
-//        Principal principal = mock(Principal.class);
-//
-//        assertEquals(HttpStatus.OK, emailAdminController
-//                .updateFeed("inputSystemId", "feedId", feedDto, principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void deleteFeed() {
-//        Principal principal = mock(Principal.class);
-//        assertEquals(HttpStatus.NO_CONTENT, emailAdminController.
-//                deleteFeed("inputSystemId", "feedId", principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void subscribeToFeed() {
-//        Principal principal = mock(Principal.class);
-//
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                subscribeToFeed("inputSystemId", "feedId", principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void unsubscribeToFeed() {
-//        Principal principal = mock(Principal.class);
-//        assertEquals(HttpStatus.NO_CONTENT, emailAdminController.
-//                unsubscribeToFeed("inputSystemId", "feedId", principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void addFeedToEmailTemplate() {
-//        Principal principal = mock(Principal.class);
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                addFeedToEmailTemplate("inputSystemId", "emailTemplateId", "feedId", principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void getFeeds() {
-//        Principal principal = mock(Principal.class);
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                getFeeds("inputSystemId", principal).getStatusCode());
-//    }
-//
-//    @Test
-//    public void getFeedByInputSystemIdAndFeedId() {
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                getFeedByInputSystemIdAndFeedId("inputSystemId", "feedId").getStatusCode());
-//    }
-//
-//    @Test
-//    public void postFeed() {
-//        Principal principal = mock(Principal.class);
-//        assertEquals(HttpStatus.OK, emailAdminController.
-//                postFeed("inputSystemId", "feedId", new EmailProviderProperties(), principal).getStatusCode());
-//    }
-//
-//}
-//
-//
+package tech.nosy.nosyemail.nosyemail.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import tech.nosy.nosyemail.nosyemail.dto.EmailTemplateDto;
+import tech.nosy.nosyemail.nosyemail.dto.InputSystemDto;
+import tech.nosy.nosyemail.nosyemail.model.EmailProviderProperties;
+import tech.nosy.nosyemail.nosyemail.model.EmailTemplate;
+import tech.nosy.nosyemail.nosyemail.model.ReadyEmail;
+import tech.nosy.nosyemail.nosyemail.service.EmailTemplateService;
+import tech.nosy.nosyemail.nosyemail.service.InputSystemService;
+
+import java.security.Principal;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailAdminControllerTest {
+
+    @InjectMocks
+    EmailAdminController emailAdminController;
+
+    @Mock
+    EmailTemplateService emailTemplateService;
+
+    @Mock
+    InputSystemService inputSystemService;
+
+
+    @Test
+    public void emailTemplatePost() {
+        EmailTemplateDto emailTemplateDto=new EmailTemplateDto();
+        emailTemplateDto.setSubject("TestSubject");
+        Principal principal=mock(Principal.class);
+        assertEquals(HttpStatus.OK, emailAdminController.
+                emailTemplatePost("dasda", "dasdas", null,
+                        principal).getStatusCode());
+        assertEquals("TestSubject",emailTemplateDto.getSubject());
+    }
+
+    @Test
+    public void emailPost() {
+        ReadyEmail readyEmail = new ReadyEmail();
+        readyEmail.setEmailTemplate(new EmailTemplate());
+        readyEmail.getEmailTemplate().setEmailTemplateSubject("TestSubject");
+
+        assertEquals(HttpStatus.OK, emailAdminController.emailPost(readyEmail).getStatusCode());
+    }
+
+    @Test
+    public void newType() {
+        InputSystemDto inputSystemDto=new InputSystemDto();
+        Principal principal=mock(Principal.class);
+        assertEquals(HttpStatus.CREATED, emailAdminController.newType(inputSystemDto,principal).getStatusCode());
+
+    }
+
+    @Test
+    public void getInputSystems() {
+        Principal principal=mock(Principal.class);
+        assertEquals(HttpStatus.OK, emailAdminController.getInputSystems(principal).getStatusCode());
+    }
+
+    @Test
+    public void getEmailAllProviders() {
+        Principal principal=mock(Principal.class);
+        when(emailTemplateService.getAllEmailProviders()).thenReturn(null);
+        assertEquals(HttpStatus.OK, emailAdminController.getEmailAllProviders(principal).getStatusCode());
+    }
+
+    @Test
+    public void newEmailTemplate() {
+        Principal principal=mock(Principal.class);
+        assertEquals(HttpStatus.CREATED, emailAdminController.
+                newEmailTemplate(principal, "inputSystemId",null).getStatusCode());
+    }
+
+    @Test
+    public void getEmailTemplateByInputSystemAndEmailTemplateId() {
+        Principal principal=mock(Principal.class);
+        assertEquals(HttpStatus.OK, emailAdminController.
+                getEmailTemplateByInputSystemAndEmailTemplateId("emailTemplateId","inputSystemId",principal).getStatusCode());
+    }
+
+    @Test
+    public void updateEmailTemplate() {
+        Principal principal=mock(Principal.class);
+
+        assertEquals(HttpStatus.OK, emailAdminController.
+                updateEmailTemplate( "inputSystemId","emailTemplateId", null, principal)
+                .getStatusCode());
+    }
+
+
+    @Test
+    public void getEmailTemplates() {
+        Principal principal=mock(Principal.class);
+
+        assertEquals(HttpStatus.OK, emailAdminController.
+                getEmailTemplates("inputSystemId", principal).getStatusCode());
+    }
+
+    @Test
+    public void deleteEmailTemplate() {
+        Principal principal=mock(Principal.class);
+
+        assertEquals(HttpStatus.NO_CONTENT, emailAdminController.
+                deleteEmailTemplate("inputSystemId", "emailTemplateId",principal).getStatusCode());
+    }
+
+    @Test
+    public void deleteInputSystem() {
+        Principal principal=mock(Principal.class);
+
+        assertEquals(HttpStatus.NO_CONTENT, emailAdminController.
+                deleteInputSystem("inputSystemId", principal).getStatusCode());
+    }
+}
+
+
--- src/test/java/tech/nosy/nosyemail/nosyemail/service/EmailServiceTest.java
+++ src/test/java/tech/nosy/nosyemail/nosyemail/service/EmailServiceTest.java
@@ -1,115 +1,115 @@
-//package tech.nosy.nosyemail.nosyemail.service;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import tech.nosy.nosyemail.nosyemail.model.*;
-//
-//import java.util.ArrayList;
-//import java.util.HashSet;
-//import java.util.List;
-//import java.util.Set;
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.doNothing;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//public class EmailServiceTest {
-//
-//  @InjectMocks
-//  EmailService emailServiceListener;
-//
-//  @Mock
-//  EmailSenderService emailService;
-//
-//
-//  private ReadyEmail readyEmail;
-//  private EmailTemplate emailTemplate;
-//  private List<PlaceHolder> placeHoldersList=new ArrayList<>();
-//  @Before
-//  public void setUp(){
-//    emailTemplate=new EmailTemplate();
-//    PlaceHolder placeHolders = new PlaceHolder();
-//    placeHolders.setName("name");
-//    placeHolders.setValue("value");
-//    placeHoldersList.add(placeHolders);
-//    emailTemplate.setEmailTemplateName("emailTemplateName");
-//    emailTemplate.setEmailTemplateFromAddress("test@nosy.tech");
-//    emailTemplate.setEmailTemplateId("emailTemplateId");
-//    Set<String> emailTemplateToSet=new HashSet<>();
-//    String emailTemplateTo="nosy@email.to";
-//    String emailTemplateTo1="test@email.to";
-//    emailTemplateToSet.add(emailTemplateTo);
-//    emailTemplateToSet.add(emailTemplateTo1);
-//
-//
-//
-//    Set<String> emailTemplateCcSet=new HashSet<>();
-//    String emailTemplateCc="nosy@email.to";
-//    emailTemplateCcSet.add(emailTemplateCc);
-//
-//
-//
-//    emailTemplate.setEmailTemplateCc(emailTemplateCcSet);
-//    emailTemplate.setEmailTemplateTo(emailTemplateToSet);
-//    emailTemplate.setEmailTemplatePriority(1);
-//    emailTemplate.setEmailTemplateRetryPeriod(1);
-//    emailTemplate.setEmailTemplateRetryTimes(1);
-//    emailTemplate.setEmailTemplateSubject("subject");
-//    emailTemplate.setEmailTemplateText("text");
-//
-//
-//  }
-//
-//
-//  @org.junit.Test(expected = org.junit.Test.None.class)
-//  public void handleReadyEmail() {
-//    readyEmail=new ReadyEmail();
-//    emailTemplate.setEmailTemplateFromProvider(EmailFromProvider.DEFAULT);
-//    readyEmail.setEmailTemplate(emailTemplate);
-//    doNothing().when(emailService).send(any(), any());
-//    emailServiceListener.handleReadyEmail(readyEmail);
-//  }
-//
-//  @org.junit.Test(expected = org.junit.Test.None.class)
-//  public void handleReadyEmailYandex() {
-//    readyEmail=new ReadyEmail();
-//    emailTemplate.setEmailTemplateFromProvider(EmailFromProvider.YANDEX);
-//    readyEmail.setEmailTemplate(emailTemplate);
-//    emailServiceListener.handleReadyEmail(readyEmail);
-//  }
-//
-//  @org.junit.Test(expected = org.junit.Test.None.class)
-//  public void handleReadyEmailGmail() {
-//    readyEmail=new ReadyEmail();
-//    emailTemplate.setEmailTemplateFromProvider(EmailFromProvider.GMAIL);
-//    readyEmail.setEmailTemplate(emailTemplate);
-//    doNothing().when(emailService).send(any(), any());
-//    emailServiceListener.handleReadyEmail(readyEmail);
-//  }
-//  @org.junit.Test(expected = Test.None.class)
-//  public void checkEmailTemplate() {
-//    EmailProviderProperties emailProviderProperties=new EmailProviderProperties();
-//    emailProviderProperties.setUsername("username");
-//    emailProviderProperties.setPassword("password");
-//    readyEmail=new ReadyEmail();
-//    readyEmail.setEmailTemplate(emailTemplate);
-//    emailTemplate.setEmailTemplateFromProvider(EmailFromProvider.GMAIL);
-//
-//    emailProviderProperties.setPlaceholders(placeHoldersList);
-//
-//    readyEmail.setEmailProviderProperties(emailProviderProperties);
-//    assertEquals("emailTemplateId", emailTemplate.getEmailTemplateId());
-//    assertEquals("emailTemplateName", emailTemplate.getEmailTemplateName());
-//    assertEquals(1, emailTemplate.getEmailTemplateRetryTimes());
-//    assertEquals(1, emailTemplate.getEmailTemplateRetryPeriod());
-//    assertEquals(1, emailTemplate.getEmailTemplatePriority());
-//    assertEquals("name", emailProviderProperties.getPlaceholders().get(0).getName());
-//    assertEquals("value", emailProviderProperties.getPlaceholders().get(0).getValue());
-//
-//  }
-//
-//}
+package tech.nosy.nosyemail.nosyemail.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import tech.nosy.nosyemail.nosyemail.model.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmailServiceTest {
+
+  @InjectMocks
+  EmailService emailServiceListener;
+
+  @Mock
+  EmailSenderService emailService;
+
+
+  private ReadyEmail readyEmail;
+  private EmailTemplate emailTemplate;
+  private List<PlaceHolder> placeHoldersList=new ArrayList<>();
+  @Before
+  public void setUp(){
+    emailTemplate=new EmailTemplate();
+    PlaceHolder placeHolders = new PlaceHolder();
+    placeHolders.setName("name");
+    placeHolders.setValue("value");
+    placeHoldersList.add(placeHolders);
+    emailTemplate.setEmailTemplateName("emailTemplateName");
+    emailTemplate.setEmailTemplateFromAddress("test@nosy.tech");
+    emailTemplate.setEmailTemplateId("emailTemplateId");
+    Set<String> emailTemplateToSet=new HashSet<>();
+    String emailTemplateTo="nosy@email.to";
+    String emailTemplateTo1="test@email.to";
+    emailTemplateToSet.add(emailTemplateTo);
+    emailTemplateToSet.add(emailTemplateTo1);
+
+
+
+    Set<String> emailTemplateCcSet=new HashSet<>();
+    String emailTemplateCc="nosy@email.to";
+    emailTemplateCcSet.add(emailTemplateCc);
+
+
+
+    emailTemplate.setEmailTemplateCc(emailTemplateCcSet);
+    emailTemplate.setEmailTemplateTo(emailTemplateToSet);
+    emailTemplate.setEmailTemplatePriority(1);
+    emailTemplate.setEmailTemplateRetryPeriod(1);
+    emailTemplate.setEmailTemplateRetryTimes(1);
+    emailTemplate.setEmailTemplateSubject("subject");
+    emailTemplate.setEmailTemplateText("text");
+
+
+  }
+
+
+  @org.junit.Test(expected = org.junit.Test.None.class)
+  public void handleReadyEmail() {
+    readyEmail=new ReadyEmail();
+    emailTemplate.setEmailTemplateFromProvider(EmailFromProvider.DEFAULT);
+    readyEmail.setEmailTemplate(emailTemplate);
+    doNothing().when(emailService).send(any(), any());
+    emailServiceListener.handleReadyEmail(readyEmail);
+  }
+
+  @org.junit.Test(expected = org.junit.Test.None.class)
+  public void handleReadyEmailYandex() {
+    readyEmail=new ReadyEmail();
+    emailTemplate.setEmailTemplateFromProvider(EmailFromProvider.YANDEX);
+    readyEmail.setEmailTemplate(emailTemplate);
+    emailServiceListener.handleReadyEmail(readyEmail);
+  }
+
+  @org.junit.Test(expected = org.junit.Test.None.class)
+  public void handleReadyEmailGmail() {
+    readyEmail=new ReadyEmail();
+    emailTemplate.setEmailTemplateFromProvider(EmailFromProvider.GMAIL);
+    readyEmail.setEmailTemplate(emailTemplate);
+    doNothing().when(emailService).send(any(), any());
+    emailServiceListener.handleReadyEmail(readyEmail);
+  }
+  @org.junit.Test(expected = Test.None.class)
+  public void checkEmailTemplate() {
+    EmailProviderProperties emailProviderProperties=new EmailProviderProperties();
+    emailProviderProperties.setUsername("username");
+    emailProviderProperties.setPassword("password");
+    readyEmail=new ReadyEmail();
+    readyEmail.setEmailTemplate(emailTemplate);
+    emailTemplate.setEmailTemplateFromProvider(EmailFromProvider.GMAIL);
+
+    emailProviderProperties.setPlaceholders(placeHoldersList);
+
+    readyEmail.setEmailProviderProperties(emailProviderProperties);
+    assertEquals("emailTemplateId", emailTemplate.getEmailTemplateId());
+    assertEquals("emailTemplateName", emailTemplate.getEmailTemplateName());
+    assertEquals(1, emailTemplate.getEmailTemplateRetryTimes());
+    assertEquals(1, emailTemplate.getEmailTemplateRetryPeriod());
+    assertEquals(1, emailTemplate.getEmailTemplatePriority());
+    assertEquals("name", emailProviderProperties.getPlaceholders().get(0).getName());
+    assertEquals("value", emailProviderProperties.getPlaceholders().get(0).getValue());
+
+  }
+
+}
