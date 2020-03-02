a88ce8e556ce92b2149fd989979c782b827a19d5
oktayalizada <oktay.alizada@gmail.com>
2020-03-02 01:06:56 +0100

    Add email config tests

---
+++ src/test/java/tech/nosy/nosyemail/nosyemail/config/RestConfigurationTest.java
@@ -0,0 +1,25 @@
+package tech.nosy.nosyemail.nosyemail.config;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RestConfigurationTest {
+
+    @InjectMocks
+    private RestConfiguration restConfiguration;
+
+    @Test(expected = Test.None.class)
+    public void corsFilter() {
+        restConfiguration.corsFilter();
+    }
+
+    @Test(expected = Test.None.class)
+    public void apiDocket() {
+        restConfiguration.apiDocket();
+
+    }
+}
